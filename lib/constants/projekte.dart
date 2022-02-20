0c9cafab6869eac7677951924d56cb63febed9c3
9katirci <berkkan.katirci@studium.uni-hamburg.de>
2022-02-20 22:14:24 +0100

    Projekt hinzugefügt

---
+++ lib/constants/projekte.dart
@@ -0,0 +1,61 @@
+class Projekt {
+  final String name;
+  final String bild;
+  final String beschreibung;
+  final String programmierSprache;
+  final String datum;
+
+  Projekt(this.name, this.bild, this.beschreibung, this.programmierSprache,
+      this.datum);
+}
+
+List<Projekt> projektList = [
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+  Projekt(
+      'AnimeListv2',
+      'bild',
+      'Eiusmod voluptate enim culpa eu cupidatat mollit voluptate commodo tempor ullamco nulla. Magna ullamco minim duis sunt magna nostrud amet laborum. Aute ex consectetur culpa excepteur. Sit culpa do labore occaecat nisi. Nisi fugiat dolore aute ea voluptate.',
+      'Flutter',
+      '2021'),
+];
