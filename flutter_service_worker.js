d013675a2d824c3443b2c29efccdf1833276db88
berkkan22 <berkkan22@gmail.com>
2022-04-10 18:08:51 +0200

    New Webrender html

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "35233296a9319678719f8ecf6aae04f3",
-"/": "35233296a9319678719f8ecf6aae04f3",
-"main.dart.js": "012b7d15f1f2929ff9e13eb4635d6a59",
+"index.html": "7231e24c09c52af718418c5acbde5699",
+"/": "7231e24c09c52af718418c5acbde5699",
+"main.dart.js": "c412ee24087edfeb633ef3161134aa51",
 "manifest.json": "d454795ec90f470ee481df70d1276310",
 "version.json": "12a92ef79835b40a6093bcc932b54998"
 };
